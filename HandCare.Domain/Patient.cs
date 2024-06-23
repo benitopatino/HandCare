746054dad9ffd360f30b0e858d845c8cdd8df58e
Benito <39175198+benitopatino@users.noreply.github.com>
2024-06-22 19:02:36 -0800

    Add project files and amended gitignore file

---
+++ HandCare.Domain/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandCare.Domain
+{
+    public class Patient
+    {
+        public string PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
