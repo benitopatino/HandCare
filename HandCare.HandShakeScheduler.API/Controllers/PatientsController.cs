99a2e2c38dc32b29c6dc31b6c3a7f3912c3888c7
Benito <39175198+benitopatino@users.noreply.github.com>
2024-06-22 19:32:01 -0800

    restructure folders

---
+++ HandCare.HandShakeScheduler.API/Controllers/PatientsController.cs
@@ -0,0 +1,27 @@
+﻿using HandCare.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandCare.HandShakeScheduler.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        [HttpGet("{patientId}")]
+        public async Task<ActionResult<Patient>> GetPatient(string patientId)
+        {
+
+            var patient = new Patient()
+            {
+                FirstName = "Hacksd",
+                LastName = "Hackson",
+                PatientId = patientId,
+                DateOfBirth = DateTime.Now
+            };
+
+            return patient == null ? NotFound() : Ok(patient);
+
+        }
+    }
+}
