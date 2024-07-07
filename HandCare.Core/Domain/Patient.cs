912b5cb855a354a56a5886ccbb01f6110eb08797
Benito <39175198+benitopatino@users.noreply.github.com>
2024-07-06 22:45:32 -0800

    update Patient model and reset initial migration

--- HandCare.Core/Domain/Patient.cs
+++ HandCare.Core/Domain/Patient.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HandCare.Core.Domain
 {
+
     public class Patient
     {
-        public string PatientId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid PatientId { get; set; }
+        
+        [StringLength(450)]
+        public string? FirstName { get; set; }
+        
+        [StringLength(450)]
+        public string? LastName { get; set; }
+        
+        [Column(TypeName ="date")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+
+        [RegularExpression("^\\d{10}$", ErrorMessage = "Please enter a valid phone number")]
+        [StringLength(50)]
+        public string? CellPhone { get; set; }
+
+        [StringLength(50)]
+        public string? Address { get; set; }
+        
+        [StringLength(450)]
+        public string? City { get; set; }
+
+
+        [StringLength(10)]
+        public string? State { get; set; }
+        
+        [StringLength(50)]
+        [RegularExpression("^\\d{5}(?:-\\d{4})?$\n", ErrorMessage = "Please enter a valid zipcode")]
+        public string? ZipCode { get; set; }
     }
+    
 }
