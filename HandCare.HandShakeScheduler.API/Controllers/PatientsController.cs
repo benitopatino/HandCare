f405a5f87fb4d1248925b5bf133a96564153a54a
Benito <39175198+benitopatino@users.noreply.github.com>
2024-07-08 21:33:36 -0800

    implement basic patient controller with CRUD functionality

--- HandCare.HandShakeScheduler.API/Controllers/PatientsController.cs
+++ HandCare.HandShakeScheduler.API/Controllers/PatientsController.cs
@@ -1,8 +1,9 @@
-﻿using HandCare.Core;
+﻿using System.Net;
+using HandCare.Core;
 using HandCare.Core.Domain;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HandCare.HandShakeScheduler.API
+namespace HandCare.HandShakeScheduler.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -15,7 +16,6 @@ namespace HandCare.HandShakeScheduler.API
             _workService = workService;
         }
         
-        
         [HttpPost]
         public ActionResult<Patient> Patient(Patient patient)
         {
@@ -25,5 +25,77 @@ namespace HandCare.HandShakeScheduler.API
             //    return CreatedAtAction("PostTodoItem", new { id = todoItem.Id }, todoItem);
             return  CreatedAtAction(nameof(Patient), new { id = patient.PatientId }, patient);
         }
+        
+        [HttpGet("patientId")]
+        public ActionResult<Patient> GetById(string patientId)
+        {
+            // validate the guid
+            if (Guid.TryParse(patientId, out var validPatientId))
+            {
+                var patient = _workService.Patients.Get(validPatientId);
+                if (patient != null)
+                    return patient;
+            }
+            return NotFound();
+        
+        }
+
+        [HttpGet]
+        public IEnumerable<Patient> GetAll()
+        {
+            var patients = _workService.Patients.GetAll();
+            return patients;
+        }
+
+        [HttpPut("patientId")]
+        public ActionResult Put(string patientId, Patient updatedPatient)
+        {
+            if (!string.Equals(patientId, updatedPatient.PatientId.ToString(),StringComparison.CurrentCultureIgnoreCase))
+                return BadRequest();
+            
+            if (Guid.TryParse(patientId, out var validPatientId))
+            {
+                var existingPatient = _workService.Patients.Get(validPatientId);
+                if (existingPatient != null)
+                {
+                    existingPatient.Address = updatedPatient.Address;
+                    existingPatient.City = updatedPatient.City;
+                    existingPatient.State = updatedPatient.State;
+                    existingPatient.ZipCode = updatedPatient.ZipCode;
+                    existingPatient.CellPhone = updatedPatient.CellPhone;
+                    existingPatient.FirstName = updatedPatient.FirstName;
+                    existingPatient.LastName = updatedPatient.LastName;
+                    existingPatient.DateOfBirth = updatedPatient.DateOfBirth;
+                    existingPatient.Email = updatedPatient.Email;
+                    existingPatient.Address = updatedPatient.Address;
+
+                }
+                _workService.Complete();
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(string patientId)
+        {
+            if (Guid.TryParse(patientId, out var validPatientId))
+            {
+                var patient = _workService.Patients.Get(validPatientId);
+                if (patient != null)
+                {
+                    _workService.Patients.Remove(patient);
+                    _workService.Complete();
+                    return Ok();
+                }
+            }
+
+            return NotFound();
+        }
+        
+        
+
+
     }
 }
