5db40fbe1df53e4a336afbb4e892a6ee3a49ae57
Benito <39175198+benitopatino@users.noreply.github.com>
2024-07-26 23:33:54 -0700

    complete basic crud functions

--- HandCare.HandShakeScheduler.API/Controllers/Admin/OfficesController.cs
+++ HandCare.HandShakeScheduler.API/Controllers/Admin/OfficesController.cs
@@ -1,6 +1,7 @@
 using HandCare.Core;
 using HandCare.Core.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace HandCare.HandShakeScheduler.API.Controllers.Admin
 {
@@ -13,6 +14,19 @@ namespace HandCare.HandShakeScheduler.API.Controllers.Admin
         public OfficesController(IUnitOfWork workService)
         {
             _workService = workService;
+        }
+
+        [HttpGet("officeId")]
+        public ActionResult<Office> Get(string officeId)
+        {
+            if (Guid.TryParse(officeId, out var validOfficeId))
+            {
+                var office = _workService.Offices.Get(validOfficeId);
+                if (office != null)
+                    return office;
+            }
+
+            return NotFound();
         }
         
         [HttpGet]
@@ -31,5 +45,49 @@ namespace HandCare.HandShakeScheduler.API.Controllers.Admin
             //    return CreatedAtAction("PostTodoItem", new { id = todoItem.Id }, todoItem);
             return  CreatedAtAction(nameof(Office), new { id = office.OfficeId }, office);
         }
+
+        [HttpPut("officeId")]
+        public ActionResult Put(string officeId, Office updatedOffice)
+        {
+            if (!string.Equals(officeId, updatedOffice.OfficeId.ToString(), StringComparison.CurrentCultureIgnoreCase))
+                return BadRequest();
+
+            if (Guid.TryParse(officeId, out var validOfficeId))
+            {
+                var existingOffice = _workService.Offices.Get(validOfficeId);
+                if (existingOffice != null)
+                {
+                    existingOffice.Address = updatedOffice.Address;
+                    existingOffice.City = updatedOffice.City;
+                    existingOffice.ZipCode = updatedOffice.ZipCode;
+                    existingOffice.State = updatedOffice.State;
+                    existingOffice.Closed = updatedOffice.Closed;
+                    existingOffice.Phone = updatedOffice.Phone;
+                }
+
+                _workService.Complete();
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(string officeId)
+        {
+            if (Guid.TryParse(officeId, out var validOfficeId))
+            {
+                var office = _workService.Offices.Get(validOfficeId);
+                if (office != null)
+                {
+                    _workService.Offices.Remove((office));
+                    _workService.Complete();
+                    return Ok();
+                }
+            }
+
+            return NotFound();
+        }
+        
     }
 }
